aaf734f127acd2aeac855badbc2bc63d7cfcb7a3
GiaHai <80776355+GiaHai@users.noreply.github.com>
2022-11-02 12:22:10 +0700

    sửa lỗi giáo viên điểm danh 1 ngày nhiều lần theo buổi

--- modules/web/src/com/company/truonghoc/web/screens/chamconggv/ChamconggvEdit.java
+++ modules/web/src/com/company/truonghoc/web/screens/chamconggv/ChamconggvEdit.java
@@ -5,9 +5,8 @@ import com.company.truonghoc.entity.Giaovien;
 import com.company.truonghoc.service.DulieuUserService;
 import com.company.truonghoc.service.SearchedService;
 import com.haulmont.cuba.core.global.DataManager;
-import com.haulmont.cuba.gui.components.DateField;
-import com.haulmont.cuba.gui.components.HasValue;
-import com.haulmont.cuba.gui.components.LookupField;
+import com.haulmont.cuba.gui.Notifications;
+import com.haulmont.cuba.gui.components.*;
 import com.haulmont.cuba.gui.screen.*;
 import com.company.truonghoc.entity.Chamconggv;
 import com.haulmont.cuba.security.global.UserSession;
@@ -38,17 +37,17 @@ public class ChamconggvEdit extends StandardEditor<Chamconggv> {
     protected DateField<Date> ngaylamField;
     @Inject
     protected SearchedService searchedService;
+    @Inject
+    protected Button commitAndCloseBtn;
+    @Inject
+    protected Notifications notifications;
 
     @Subscribe
     protected void onInit(InitEvent event) {
-//        donvigvField.setEditable(false);
         hotenGvField.setRequired(true);
         ngaylamField.setRequired(true);
-//        buoilamField.setRequired(true);
         List<String> buoilam = Arrays.asList("Làm cả ngày", "Ca sáng", "Ca chiều", "Ca chủ nhật", "Ca chiều 5h-6h", "Ca chiều 6h-7h");
         buoilamField.setOptionsList(buoilam);
-
-
     }
 
     @Subscribe
@@ -65,25 +64,8 @@ public class ChamconggvEdit extends StandardEditor<Chamconggv> {
         }
     }
 
-//    @Subscribe
-//    protected void onAfterShow(AfterShowEvent event) {
-//        if (!dulieuUserService.timdovi(userSession.getUser().getLogin()).getLoockup_donvi().getDonvitrungtam()) {
-//            donvigvField.setValue(dulieuUserService.timdovi(userSession.getUser().getLogin()).getLoockup_donvi());
-//            donvigvField.setEditable(false);
-//            if (dulieuUserService.timdovi(userSession.getUser().getLogin()).getGiaovien() != null) {
-//                hotenGvField.setValue(dulieuUserService.timdovi(userSession.getUser().getLogin()).getGiaovien());
-//                hotenGvField.setEditable(false);
-//            }
-//        } else {
-//            donvigvField.setOptionsList(searchedService.loaddonvi());
-//        }
-//    }
-
     @Subscribe("buoilamField")
     protected void onBuoilamFieldValueChange(HasValue.ValueChangeEvent<String> event) {
-//        tienbuoiField.setOptionsList(loadtienluongtheoca());
-
-
         if (buoilamField.getValue() == "Ca chủ nhật") {
             tienbuoiField.setValue(Integer.valueOf("100000"));
         }
@@ -98,15 +80,7 @@ public class ChamconggvEdit extends StandardEditor<Chamconggv> {
         if (buoilamField.getValue() == "Làm cả ngày" || buoilamField.getValue() == "Ca sáng" || buoilamField.getValue() == "Ca chiều") {
             tienbuoiField.clear();
         }
-
     }
-
-//    private List<TienluongtheoCa> loadtienluongtheoca(){
-//        return dataManager.load(TienluongtheoCa.class)
-//                .query("select e from truonghoc_TienluongtheoCa e where e.caLam = :calam")
-//                .parameter("calam", buoilamField.getValue())
-//                .list();
-//    }
 
     @Subscribe("donvigvField")
     protected void onDonvigvFieldValueChange(HasValue.ValueChangeEvent<Donvi> event) {
@@ -122,6 +96,38 @@ public class ChamconggvEdit extends StandardEditor<Chamconggv> {
                 .query("select e from truonghoc_Giaovien e where e.donvi = :donvitao")
                 .parameter("donvitao", donvitao)
                 .list();
+    }
+
+    public List<Chamconggv> chamCongLoadDkKhongTrung(Donvi donVi, Giaovien giaoVien, Date ngayLam, String buoiLam) {
+        String query = "select e from truonghoc_Chamconggv e ";
+        String where = "where e.donvigv = :donvi and e.hotengv = :giaovien " +
+                "and e.ngaylam = :ngaylam and e.buoilam like :buoilam";
+
+        Map<String, Object> params = new HashMap<>();
+
+        params.put("donvi", donVi);
+        params.put("giaovien", giaoVien);
+        params.put("ngaylam", ngayLam);
+        params.put("buoilam", "%"+ buoiLam +"%");
+        return dataManager.load(Chamconggv.class)
+                .query(query + where)
+                .setParameters(params)
+                .list();
+    }
+
+    @Subscribe("commitAndCloseBtn")
+    protected void onCommitAndCloseBtnClick(Button.ClickEvent event) {
+        Integer chamCongLoadDkKhongTrungLap = chamCongLoadDkKhongTrung(donvigvField.getValue(), hotenGvField.getValue(), ngaylamField.getValue(), buoilamField.getValue()).size();
+        if (chamCongLoadDkKhongTrungLap == 0){
+            closeWithCommit();
+        }else {
+            notifications.create()
+                    .withCaption("Bạn đã điểm danh cho buổi này rồi !")
+                    .withPosition(Notifications.Position.BOTTOM_RIGHT)
+                    .withType(Notifications.NotificationType.HUMANIZED)
+                    .show();
+            buoilamField.clear();
+        }
     }
 
 }
